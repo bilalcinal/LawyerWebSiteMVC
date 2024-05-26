916c861a7ae08e0093d858ae1b157ff780e023cd
Bilal Cinal <hbilalcinal@gmail.com>
2024-05-26 23:04:02 +0300

    commit transfer

--- LawyerWebSiteMVC/Controllers/HomeController.cs
+++ LawyerWebSiteMVC/Controllers/HomeController.cs
@@ -1,21 +1,25 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using LawyerWebSiteMVC.Interface;
+using Microsoft.AspNetCore.Mvc;
 
 namespace LawyerWebSiteMVC.Controllers
 {
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
+        private readonly ISliderService _sliderService;
 
-        public HomeController(ILogger<HomeController> logger)
+
+        public HomeController(ILogger<HomeController> logger , ISliderService sliderService)
         {
+            _sliderService = sliderService;
             _logger = logger;
         }
 
-        public IActionResult Index()
+         public async Task<IActionResult> Index()
         {
-            return View();
+            var sliders = await _sliderService.GetAllSlidersAsync();
+            return View(sliders);
         }
-
         public IActionResult Privacy()
         {
             return View();
